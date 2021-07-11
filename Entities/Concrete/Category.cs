6a8344b5e6914dcc34903bd2956f9da0ee5e0f34
Cemcir <enescemcir1994@gmail.com>
2021-07-11 18:17:09 +0300

    değişiklik yapıldı

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Kalıtım ve implement almayan class kalmasın
+    public class Category:IEntity
+    {
+    }
+}
