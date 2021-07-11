495ecaaf3da84e42095e4f8e94bb0a7fb2e37e9c
Cemcir <enescemcir1994@gmail.com>
2021-07-11 18:30:51 +0300

    Açıklama eklendi

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
+    // I interface olduğunu bildirir
+    // Product veritabanı tablosu olduğunu anlatır
+    // Dal hangi katmanda olduğunu anlatır
     public interface IProductDal
     {
         List<Product> GetAll();
