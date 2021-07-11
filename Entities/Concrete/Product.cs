85ed8cf35fc92c3b1bd64ac90c3673e038ec54d8
Cemcir <enescemcir1994@gmail.com>
2021-07-11 18:05:26 +0300

    değişiklik yapıldı

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Product
+    // IEntity implement eden bir class veritabanı tablosudur
+    public class Product:IEntity
     {
+
     }
 }
